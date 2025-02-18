d8137f6fc78005c70b3a0becc392138c0a0fd3a9
brayden Jones <u1098831@umail.utah.edu>
2025-02-18 16:22:24 -0700

    Main file

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,4 +1,13 @@
 import java.util.ArrayList;
 
 public class QuickSorter implements Sorter {
+    @Override
+    public void sort(ArrayList list) {
+        // TODO Auto-generated method stub
+    }
+
+    public QuickSorter(PivotChooser<E> chooser){
+
+    }
+
 }
